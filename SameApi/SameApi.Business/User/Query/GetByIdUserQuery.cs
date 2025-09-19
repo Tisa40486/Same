e80ab616a63d17ecbbaa0705b11bf5942b454d59
Mattis <lefrancadammattis@gmail.com>
2025-09-19 11:30:36 +0200

    Miaou

---
+++ SameApi/SameApi.Business/User/Query/GetByIdUserQuery.cs
@@ -0,0 +1,34 @@
+﻿using SameApi.Db.UnitOfWork;
+using AutoMapper;
+using SameApi.Dto;
+using MediatR;
+
+namespace SameApi.Business.User.Command
+{
+    public class GetByIdUserQuery: IRequest<UserResponse>
+    {
+        public int Id { get; set; }
+    }
+    public class GetPersonByIdQueryHandler : IRequestHandler<GetPersonByIdQuery, PersonResponse>
+    {
+        readonly  ISameApiUnitOfWork _apiTestUnitOfWork;
+        readonly IMapper _mapper;
+
+        public GetPersonByIdQueryHandler(
+            ISameApiUnitOfWork apiTestUnitOfWork, IMapper mapper)
+        {
+            _apiTestUnitOfWork = apiTestUnitOfWork;
+            _mapper = mapper;
+        }
+
+
+        public async Task<PersonResponse> Handle(GetPersonByIdQuery request, CancellationToken cancellationToken)
+        {
+            var data = await _apiTestUnitOfWork.PersonRepository.GetByIdAsync(request.Id);
+
+            var result = _mapper.Map<PersonResponse>(data);
+            return result;
+        }
+    }
+
+}
---
+++ SameApi/SameApi.Dto/UserResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace SameApi.Dto
+{
+    public class UserResponse
+    {
+        public int Id { get; set; }
+        public bool IsAdmin { get; set; }
+        public int Age { get; set; }
+        public required string Pseudo { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public int NumberFollowers { get; set; }
+        public DateTime CreateAt { get; set; }
+        public int GenderId { get; set; }
+        public int? GenderDao { get; set; }
+    }
+}
